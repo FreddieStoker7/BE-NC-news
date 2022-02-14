3c2b4c951510b1538084964b7fafb397b80c254f
Freddie Stoker <freddiestoker@Freddies-Air-M1.broadband>
2022-02-14 15:26:22 +0000

    completed the first endpoint GET all topics

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Northcoders News API
 
+
+A .env.development and .env.test file must be created to connect to the devlopment and test data respectively.
---
+++ controllers/app-controllers.js
@@ -0,0 +1,15 @@
+const { selectTopics } = require("../models/app-models.js");
+
+exports.getTopics = (req, res, next) => {
+  selectTopics().then((topics) => {
+    res.status(200).send({ topics: topics });
+  })
+  .catch(next);
+};
+
+exports.getArticle = (req, res, next) => {
+    const {article_id} = req.params
+    selectArticle(article_id).then((article) => {
+        res.status(200).send({article: article})
+    })
+}
